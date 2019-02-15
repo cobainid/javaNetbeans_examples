357ca64c4f8778fb736c597748273188f9a04bab
defrindr <defrindr@gmail.com>
2019-02-16 03:02:19 +0700

    add login form example

---
+++ loginForm/src/login/Login.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login;
+
+/**
+ *
+ * @author nightsec
+ */
+public class Login {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginForm Login;
+        // TODO code application logic here
+        Login = new LoginForm();
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
